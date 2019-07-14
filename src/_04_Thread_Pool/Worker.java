7c320363384003275d79f143211aae73137d8ccd
WriterArtistCoder <41657434+WriterArtistCoder@users.noreply.github.com>
2019-07-13 17:04:32 -0700

    Task WIP, ThreadPool WIP, Worker WIP (Commit @ Home)

--- src/_04_Thread_Pool/Worker.java
+++ src/_04_Thread_Pool/Worker.java
@@ -1,12 +1,24 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Worker implements Runnable {
+	
+	// WARNING
+	// MAY NOT WORK IF IT IS JUST CLONING THE TASK QUEUE
+	// INSTEAD OF USING THE SAME ONE THE OTHER WORKERS DO
+	
+	ConcurrentLinkedQueue<Task> taskQueue;
+
+	public Worker(ConcurrentLinkedQueue<Task> q) {
+		taskQueue = q;
+	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		while (!taskQueue.isEmpty()) {
+			taskQueue.remove().perform();
+		}
 	}
 
 
