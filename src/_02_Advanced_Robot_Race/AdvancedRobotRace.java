6b5f093e7453694b5b6d31aae7b5a09df48445aa
WriterArtistCoder <41657434+WriterArtistCoder@users.noreply.github.com>
2019-09-04 14:11:12 -0700

    AdvancedRobotRace

--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,5 +1,9 @@
 package _02_Advanced_Robot_Race;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Random;
 
 import javax.swing.JOptionPane;
@@ -7,19 +11,21 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class AdvancedRobotRace {
-	
+
 	static int rokuRoboot = 0;
-	
+	public int winner = -1;
+
 	static final int robotNum = 5;
 
 	static final int windowWidth = 1300;
 	static final int windowHeight = 1000;
 
+	static final int robots = 5;
 	static final int finishLine = 900;
 
 	static boolean reachedTop;
-	
-	Robot[] race = new Robot[5];
+
+	Robot[] race = new Robot[robots];
 
 	public AdvancedRobotRace() {
 		reachedTop = false;
@@ -28,47 +34,60 @@ public class AdvancedRobotRace {
 	// TODO AUGHHH! MY ROBOTS ARE CREATING FOUR MEANINGLESS EMPTY WINDOWS!
 	// AND WHEN YOU CLOSE ONE ALL THE OTHERS CLOSE! AAAA! (_/ >o<)_/ = _|_|_
 	// AND ALSO!! IT SHOWS FIVE NOTIFICATIONS FOR WHICH ROBOT WON!!
-	
 	// Less Y and X is UP and LEFT, respectively
 
 	// Re-do the robot race recipe from level 3 module 0.
 	// This time, use threads to make all of the robots go at the same time.
 
-	public static void main(String[] args) {
-		Thread[] threads = new Thread[robotNum];
-		AdvancedRobotRace robotRace = new AdvancedRobotRace();
-		
-		for (int i = 0; i < threads.length; i++) {
-			threads[i] = new Thread(()->robotRace.run());
+	public static void main(String args[]) {
+		new AdvancedRobotRace().straightRace();
+	}
+
+	public void straightRace() {
+		Thread[] armyOfThreads = new Thread[robots];
+
+		for (int i = 0; i < robots; i++) {
+			// 4. make each robot start at the bottom of the screen, side by side, facing up
+			armyOfThreads[i] = new Thread(new RunnableRobot(i));
 		}
-		
-		for (int i = 0; i < threads.length; i++) {
-			threads[i].start();
+
+		for (int i = 0; i < armyOfThreads.length; i++) {
+			armyOfThreads[i].start();
 		}
 	}
 
-	public void run() {
-		int i = rokuRoboot;
-		rokuRoboot++;
-		
-		race[i] = new Robot();
-		race[i].show();
-		
-		race[i].moveTo(i*(windowWidth/robotNum), 1000);
-		
-		while (!reachedTop) {
-			race[i].move(new Random().nextInt(200));
-			if (race[i].getY() < finishLine) {
-				reachedTop = true;
-				JOptionPane.showMessageDialog(null, "RokuRoboot #"+i+" has won!");
-			}
+	public class RunnableRobot implements Runnable {
+		// PANT PANT PANT I AM RUNNING!
+
+		Robot myRobot;
+		int myNum;
+
+		public RunnableRobot(int i) {
+			super();
+			myRobot = new Robot();
+			myRobot.setX((i+1)*(windowWidth/robots));
+			myRobot.setY(800);
+			myRobot.setSpeed(1);
 			
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+			myNum = i;
+		}
+
+		@Override
+		public void run() {
+			while (winner == -1) {
+				myRobot.move(new Random().nextInt(200));
+
+				if (myRobot.getY() <= 200) {
+					if (winner == -1) {
+						winner = myNum;
+						JOptionPane.showMessageDialog(null, "HEY GUYS I WON!!\n#AdvancedRobotRace\n#Robot"+myNum+"Won");
+					}
+					
+					break;
+				}
 			}
 		}
+
 	}
-	
+
 }
